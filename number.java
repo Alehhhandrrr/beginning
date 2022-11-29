6e1a56b0e3f3a7dd21b371063ca231a1a6a104fd
unknown <lepeshko.alexandr@mail.ru>
2022-11-29 22:52:36 +0300

    New project to push to GitHub

---
+++ number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число ");
+        int n = in.nextInt();
+        if (-10 < n && n < 10) {
+            System.out.println("Одинаковые цифры");
+        } else
+        {
+            while(true) {
+                boolean a;
+                a = (n % 10) == ((n / 10) % 10);
+                if(!a){
+                    System.out.print("Разные цифры");
+                    break;
+                }else{
+                    System.out.print("Одинаковые цифры");
+                    break;
+                }
+            }
+        }
+    }
+}
